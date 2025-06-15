8f7c2096dc6752d990241be8b61ce71ab1dac275
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-06-15 10:04:44 +0200

    Update project structure: rename package, add custom styles, enhance homepage layout, and implement hero section

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,25 +2,28 @@
 
 @section('title', 'Homepage')
 @php
-$cards= config('comics');
+    $cards = config('comics');
 @endphp
 
 @section('content')
-
-<div class="container py-5 text-center">
-    <div class="row g-4">
-        @foreach ($cards as $item)
-            <div class="col-md-4 col-lg-2">
-                <div class="card card_bg ">
-                    <img src="{{ $item['thumb'] }}" class="card-img-top" alt="{{ $item['title'] }}" />
-                    <div class="card-body">
-                        <p class="card-text text-center">{{ $item['title'] }}</p>
+    @include ('.partials.hero')
+    <div class="container py-5 position-relative ">
+        <div class="flag">
+            Current series
+        </div>
+        <div class="row g-4">
+            @foreach ($cards as $item)
+                <div class="col-md-4 col-lg-2">
+                    <div class="card card_bg ">
+                        <img src="{{ $item['thumb'] }}" class="card-img-top" alt="{{ $item['title'] }}" />
+                        <div class="card-body">
+                            <p class="card-text text-center">{{ $item['title'] }}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
+        <div class="btn btn-primary rounded-0 "> Load more</div>
     </div>
-    <div class="btn btn-primary rounded-0 "> Load more</div>
-</div>
-@include('partials.banner')
+    @include('partials.banner')
 @endsection
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    @vite('resources/css/app.css')
 </head>
+
 <body>
     @include('partials.header')
 
@@ -16,4 +17,5 @@
 
     @include('partials.footer')
 </body>
+
 </html>
